45b924ac178e9ccc04093f4ec26a6561fe3ecc12
alison545 <malison.am38@gmail.com>
2020-07-12 14:44:54 -0300

    Transformar los numeros a entero y sumar

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,15 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	var num1 ;
+	var num2;
+	var resultado;
+
+	num1 = document.getElementById("txtIdNumeroUno").value;
+	num2 = document.getElementById("txtIdNumeroDos").value;
+    resultado = parseInt(num1) + parseInt(num2);
+
+
+	alert(resultado);
 }
 
