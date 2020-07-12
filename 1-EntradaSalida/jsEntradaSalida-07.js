20681995265f7bcb6f880b1d862f97fb944abd97
alison545 <malison.am38@gmail.com>
2020-07-12 14:58:41 -0300

    Operaciones matematicas

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,21 +5,33 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	var num1 =document.getElementById("txtIdNumeroUno").value;
+	var num2=document.getElementById("txtIdNumeroDos").value;
+    var resultado = parseInt(num1) + parseInt(num2);
+	alert("La suma es " +resultado);	
 }
 
 function restar()
 {
-	
+	var num1 =document.getElementById("txtIdNumeroUno").value;
+	var num2=document.getElementById("txtIdNumeroDos").value;
+	var resultado = parseInt(num1) - parseInt(num2);
+	alert("La resta es " +resultado);
 }
 
 function multiplicar()
 { 
-	
+	var num1 =document.getElementById("txtIdNumeroUno").value;
+	var num2=document.getElementById("txtIdNumeroDos").value;
+	var resultado = parseInt(num1) * parseInt(num2);
+	alert("La multiplicacion nos da " +resultado);
 }
 
 function dividir()
 {
-	
+	var num1 =document.getElementById("txtIdNumeroUno").value;
+	var num2=document.getElementById("txtIdNumeroDos").value;
+	var resultado = parseInt(num1) / parseInt(num2);
+	alert("La division da " +resultado);
 }
 
